210ef7a6c773b732006004be3f8d5708cf4eb294
Jorge Izquierdo <jorge@izqui.me>
2018-08-30 18:24:44 +0200

    Reorg tests

--- test/liquid-democracy.js
+++ test/liquid-democracy.js
@@ -9,7 +9,7 @@ const pct16 = x => new web3.BigNumber(x).times(new web3.BigNumber(10).toPower(16
 const createdVoteId = receipt => receipt.logs.filter(x => x.event == 'StartVote')[0].args.voteId
 
 contract('Liquid Democracy', accounts => {
-  let staking, ld
+  let staking, ld, voteId
 
   const A = accounts[1]
   const B = accounts[2]
@@ -38,36 +38,54 @@ contract('Liquid Democracy', accounts => {
       await staking.addFakeStake(addr, STAKES[addr])
     }
   })
+  const createVote = async (from = A) => (
+    createdVoteId(await ld.newVote(EMPTY_SCRIPT, '', false, { from }))
+  )
 
-  context.only('on vote', () => {
-    const createVote = async (from = A) => (
-      createdVoteId(await ld.newVote(EMPTY_SCRIPT, '', false, { from }))
-    )
+  const assertVoteResult = async (voteId, yea, nay, stage) => {
+    const voteInfo = await ld.getVote(voteId)
 
-    const assertVoteResult = async (voteId, yea, nay, stage) => {
-      const voteInfo = await ld.getVote(voteId)
+    assert.equal(voteInfo[6], yea, 'yea count should have been correct' + stage ? ` for ${stage}` : '')
+    assert.equal(voteInfo[7], nay, 'nay count should have been correct' + stage ? ` for ${stage}` : '')
+  }
 
-      assert.equal(voteInfo[6], yea, 'yea count should have been correct' + stage ? ` for ${stage}` : '')
-      assert.equal(voteInfo[7], nay, 'nay count should have been correct' + stage ? ` for ${stage}` : '')
-    }
-
-    it('simple vote count', async () => {
+  context('1-hop delegation chain', () => {
+    // Delegation and vote creation done in beforeEach don't count for the gas analysis of the test case
+    beforeEach(async () => {
       await delegate(A, B)
 
-      const voteId = await createVote()
+      voteId = await createVote()
+    })
 
+    it('simple vote count', async () => {
       await vote(B, voteId, true)
       await vote(C, voteId, false)
 
       await assertVoteResult(voteId, STAKES[A] + STAKES[B], STAKES[C])
     })
 
-    it('allows overruling', async () => {
+    it('delegate can change vote after overruling', async () => {
+      await vote(B, voteId, true)
+      await vote(A, voteId, false)
+      await assertVoteResult(voteId, STAKES[B], STAKES[A], 'after A overrules')
+      
+      await vote(B, voteId, false)
+      await assertVoteResult(voteId, 0, STAKES[A] + STAKES[B], 'after B changes vote')
+      
+      await vote(A, voteId, true)
+      await assertVoteResult(voteId, STAKES[A], STAKES[B], 'after A changes vote')
+    })
+  })
+
+  context('2-hop delegation chain', () => {
+    beforeEach(async () => {
       await delegate(A, B)
       await delegate(B, C)
 
-      const voteId = await createVote()
+      voteId = await createVote()
+    })
 
+    it('allows overruling', async () => {
       await vote(C, voteId, true)
       await assertVoteResult(voteId, STAKES[A] + STAKES[B] + STAKES[C], 0, 'after C votes')
 
@@ -79,11 +97,6 @@ contract('Liquid Democracy', accounts => {
     })
 
     it('avoids delegate vote by overruling early', async () => {
-      await delegate(A, B)
-      await delegate(B, C)
-
-      const voteId = await createVote()
-
       await vote(A, voteId, true)
       await assertVoteResult(voteId, STAKES[A], 0, 'after A votes')
 
@@ -92,22 +105,6 @@ contract('Liquid Democracy', accounts => {
 
       await vote(C, voteId, false)
       await assertVoteResult(voteId, STAKES[A], STAKES[B] + STAKES[C], 0, 'after C votes')
-    })
-
-    it('delegate can change vote after overruling', async () => {
-      await delegate(A, B)
-
-      const voteId = await createVote()
-
-      await vote(B, voteId, true)
-      await vote(A, voteId, false)
-      await assertVoteResult(voteId, STAKES[B], STAKES[A], 'after A overrules')
-      
-      await vote(B, voteId, false)
-      await assertVoteResult(voteId, 0, STAKES[A] + STAKES[B], 'after B changes vote')
-      
-      await vote(A, voteId, true)
-      await assertVoteResult(voteId, STAKES[A], STAKES[B], 'after A changes vote')
     })
   })
 })
